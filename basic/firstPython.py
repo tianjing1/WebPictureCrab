3fb8fb09290f5eff66abeb98c7b23a2373efea8e
surenano <qsqsqsqsqssure@163.com>
2018-07-17 23:53:47 +0800

    使用正则后下载网页中所有图片

--- basic/firstPython.py
+++ basic/firstPython.py
@@ -1,15 +1,28 @@
-import urllib.request
+import urllib
+import re
 
-# page = urllib.request.urlopen('http://www.baidu.com')
+# page = urllib.urlopen('http://www.baidu.com')
 # html = page.read()
 # print(html)
 #
 # pageFile = open('pageCode.txt', 'wb+')
 # pageFile.write(html)
 # pageFile.close()
-
+#
 def get_html(url):
-    page = urllib.request.urlopen('http://www.baidu.com')
+    page = urllib.urlopen(url)
     html = page.read()
     return html
 
+def getImg(html):
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imageList = re.findall(imgre , html)
+    x=0
+    for imgurl in imageList:
+        urllib.urlretrieve(imgurl, '%s.jpg' % x)
+        x+=1
+    return imageList
+
+html = get_html("http://tieba.baidu.com/p/2460150866")
+print getImg(html)
