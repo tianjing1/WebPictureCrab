01e129bae9a6bdac43e5e0f3840babc2750b1321
surenano <qsqsqsqsqssure@163.com>
2018-09-28 15:51:14 +0800

    更新git跟踪文件

--- basic/downloadPicture.py
+++ basic/downloadPicture.py
@@ -1,28 +1,24 @@
-import urllib
-import re
 
-# page = urllib.urlopen('http://www.baidu.com')
-# html = page.read()
-# print(html)
-#
-# pageFile = open('pageCode.txt', 'wb+')
-# pageFile.write(html)
-# pageFile.close()
-#
+import re
+import urllib
+
+
 def get_html(url):
     page = urllib.urlopen(url)
     html = page.read()
     return html
 
+
 def getImg(html):
     reg = r'src="(.+?\.jpg)" pic_ext'
     imgre = re.compile(reg)
-    imageList = re.findall(imgre , html)
-    x=0
+    imageList = re.findall(imgre, html)
+    x = 0
     for imgurl in imageList:
         urllib.urlretrieve(imgurl, '%s.jpg' % x)
-        x+=1
+        x += 1
     return imageList
 
+
 html = get_html("http://tieba.baidu.com/p/2460150866")
-print getImg(html)
+print(getImg(html))
